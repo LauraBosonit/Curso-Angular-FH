a948c4ba32d8cde0e6c9dcc075ddc7b75549c00a
LauraBosonit <laura.cabezas@bosonit.com>
2023-05-12 12:09:27 +0200

    Proyecto pipes terminado

--- 05-pipes-app/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipes-app/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./uncommon-page.component.css']
 })
 export class UncommonPageComponent {
+  //18nSelect
+  public name: string = 'Laura';
+  public gender: 'male'|'female' = 'female';
+
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  changeClient():void {
+    this.name = 'Flavio';
+    this.gender = 'male';
+  }
+
+  //i18nPlural
+  public clients: string[] = ['Laura', 'Flavio', 'Daniel', 'Ángeles', 'Antonio', 'Marta', 'Gabriela'];
+
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos 2 clientes esperando',
+    'other': 'tenemos # clientes esperando'
+  }
+
+  deleteClient(): void {
+    this.clients.shift();
+  }
+
+  //KeyValue Pipe
+  public person = {
+    name: 'Laura',
+    age: 36,
+    addres: 'Jaén, España'
+  }
+
+  //Async Pipe
+  public myObservableTimer = interval(2000).pipe(tap( value => console.log('tap:', value)));
+
+  public promiseValue: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data en la promesa.')
+      console.log('Tenemos data en la promesa.');
+      this.person.name = 'Otro nombre';
+    }, 3500);
+  })
 
 }
